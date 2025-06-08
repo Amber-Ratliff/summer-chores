2e8cceb21172422ef7a88d1183b4d12f7e2c2e80
Amber Ratliff <aratliff1112@gmail.com>
2025-06-08 17:03:10 -0400

    added async/await version, turned the functions into an async function

---
+++ asyncAwaitVersion.js
@@ -0,0 +1,91 @@
+//used Math.ceil because I thought it might give a better shot at completion
+const complete = Math.ceil(Math.random() * 10);
+
+function mowYard(person) {
+    return new Promise(resolve => {
+        setTimeout(() => { 
+            resolve(`${person} mowed the yard.`)
+    }, 2000);
+    });   
+}
+
+function weedEat(person) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            //the threshold to beat increases in each function to simulate the person getting more tired
+            //starts at 2 because it will always be at least 1
+            if (complete >= 2) {
+                resolve(`${person} finished using the weed eater.`)
+         } else {
+                reject(`${person} fell asleep after mowing the yard.`)
+         }
+        }, 1500)
+    });
+}
+
+function trimHedges(person) {
+   return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (complete >= 3) {
+                resolve(`${person} finished trimming the hedges.`)
+         } else {
+                reject(`${person} fell asleep after using the weed eater.`)
+         }
+        }, 1000)
+    });
+
+   
+}
+
+function collectWood(person) {
+     return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (complete >= 4) {
+                resolve(`${person} finished collecting wood.`)
+         } else {
+                reject(`${person} fell asleep after trimming the hedges.`)
+         }
+        }, 2500)
+    });   
+}
+
+function waterGarden(person) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (complete >= 5) {
+                resolve(`${person} finished watering the garden.`)
+         } else {
+                reject(`${person} fell asleep after collecting wood.`)
+         }
+        }, 500)
+    });   
+}
+
+async function doSummerChores() {
+    const person = 'Alice';
+    try {
+        const mowYardResult = await mowYard(person, complete);
+        console.log(mowYardResult);
+
+        const weedEatResult = await weedEat(person, complete);
+        console.log(weedEatResult);
+
+        const trimHedgesResult = await trimHedges(person, complete);
+        console.log(trimHedgesResult);
+
+        const collectWoodResult = await collectWood(person, complete);
+        console.log(collectWoodResult);
+
+        const waterGardenResult = await waterGarden(person, complete);
+        console.log(waterGardenResult);
+
+        console.log(`${person} finished all the chores!`)
+    }
+
+    catch (error) {
+        console.error(error);
+    }
+    
+}
+
+doSummerChores();
--- readme.md
+++ readme.md
@@ -12,3 +12,5 @@ The probability of John falling asleep increases with each chore he completes.
 Changed the probability simulation to a whole number and declared it outside of the functions so that the same number is used throughout each function. The threshold gets higher in each function to simulate the person getting more tired throught the day.
 
 Added promiseVersion.js to change the callback functions to promises to make the code cleaner and easier to read.
+
+Added asyncAwaitVersion.js and turned the functions into async/await to make the code easier to read.
